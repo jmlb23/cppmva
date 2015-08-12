c460eaeacae514c34fca5863627ecfb487b93db6
jmlb23 <contadoinstituto@gmail.com>
2015-08-12 18:53:02 +0200

    herencia incompleta

---
+++ herencia/Rectangulo.h
@@ -0,0 +1,12 @@
+class Rectangulo
+{
+public:
+	Rectangulo();
+	Rectangulo(int, int);
+	int getAlto();
+	int getAncho();
+	
+private:
+	int _altura;
+	int _ancho;
+};
---
+++ herencia/implementacionR.cpp
@@ -0,0 +1,8 @@
+#include "Rectangulo.h"
+
+Rectangulo::Rectangulo() : _altura{0}, _ancho{0}
+{
+
+}
+
+
